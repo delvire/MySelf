19f7d18dcfdec496e3b1f415272b8427825ca3da
alazarg8 <hi@alazarg8.me>
2017-10-02 17:33:31 -0400

    Initial commit

---
+++ src/MySelf.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+    public class MySelf
+    {
+
+        public static void main(String arg[])
+        {
+
+                String name;
+                String experiance;
+                String major;
+                String interested;
+                String origin;
+
+
+
+        System.out.print("My name is Alazar.");
+    System.out.print("I have beginner experience in Java.");
+    System.out.print("My Major is Computer Science.");
+    System.out.print("Something interesting about me is I can't say No to people.");
+    System.out.print("My origin is Ethiopia.");
+
+    }
+}
